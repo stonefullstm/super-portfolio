3623caa439f9bcb1ba0cc628ebd1d6479c80bea6
stonefullstm <stonefull.stm@gmail.com>
2023-11-29 19:31:43 -0300

    Requisito 3 - Cria um C.R.U.D para Project

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import ProfileViewSet
+from .views import ProfileViewSet, ProjectViewSet
 
 router = routers.DefaultRouter()
 router.register(r'profiles', ProfileViewSet)
+router.register(r'projects', ProjectViewSet)
 
 urlpatterns = [
   path('', include(router.urls))
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
-from .models import Profile
-from .serializers import ProfileSerializer
+from .models import Profile, Project
+from .serializers import ProfileSerializer, ProjectSerializer
 
 
 # Create your views here.
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
